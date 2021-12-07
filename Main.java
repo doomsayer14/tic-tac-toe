d4b7178a6c2cf5136df80fc6e9d6a9097aef6246
vyesman <86164269+doomsayer14@users.noreply.github.com>
2021-12-07 14:16:07 +0200

    1/6 done ✓

--- Main.java
+++ Main.java
@@ -23,7 +23,7 @@ public class Main {
 
     //getters and setters
 
-    //-----------------------------//
+    //-----------------------------// 
     public String getInputLine() {
         return inputLine;
     }
