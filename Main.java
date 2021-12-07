44cf3283aaac676a83ee52736bb07b4dfab6dade
vyesman <86164269+doomsayer14@users.noreply.github.com>
2021-12-07 14:58:10 +0200

    2/5 temporally save the code (rough draft)

--- Main.java
+++ Main.java
@@ -15,48 +15,60 @@ public class Main {
     //word, that we have to search for
     private String specWord;
 
+    //number of people, which user can type in
+    private int numOfPeople;
+
+    private int numOfQueries;
+
     public Main() {
         scanner = new Scanner(System.in);
         inputLine = "";
         specWord = "";
+        numOfPeople = 0;
+        numOfQueries = 0;
     }
 
     //getters and setters
 
-    //-----------------------------// 
-    public String getInputLine() {
-        return inputLine;
-    }
+    //-----------------------------//
+    public String getInputLine() {return inputLine;}
 
-    public void setInputLine(String inputLine) {
-        this.inputLine = inputLine;
-    }
+    public void setInputLine(String inputLine) {this.inputLine = inputLine;}
 
-    public String[] getWords() {
-        return words;
-    }
+    //впадлу обращаться к массиву через гетер
+    //public String[] getWords() { return words; }
 
-    public void setWords(String[] words) {
-        this.words = words;
-    }
+    public void setWords(String[] words) {this.words = words;}
 
-    public String getSpecWord() {
-        return specWord;
-    }
+    public String getSpecWord() {return specWord;}
 
-    public void setSpecWord(String specWord) {
-        this.specWord = specWord;
-    }
+    public void setSpecWord(String specWord) {this.specWord = specWord;}
+
+    public int getNumOfPeople() {return numOfPeople;}
+
+    public void setNumOfPeople(int numOfPeople) {this.numOfPeople = numOfPeople;}
+
+    public int getNumOfQueries() {return numOfQueries;}
+
+    public void setNumOfQueries(int numOfQueries) {this.numOfQueries = numOfQueries;}
+
     //-----------------------------//
 
     //start of the program. Calls method "process()",
     //all logic described here
-    public static void main(String[] args) {
-        new Main().process();
-    }
+    public static void main(String[] args) {new Main().process();}
 
     //main method
     private void process() {
+        System.out.println("Enter the number of people:");
+        setNumOfPeople(scanner.nextInt());
+        System.out.println("Enter the number of search queries:");
+        setNumOfQueries(scanner.nextInt());
+
+        for (int i = 0; i<getNumOfQueries();i++) {
+            System.out.println("Enter data to search people:");
+        }
+
         setInputLine(scanner.nextLine());
         setWords(getInputLine().split(" "));
         setSpecWord(scanner.next());
@@ -69,5 +81,3 @@ public class Main {
         System.out.println("Not found");
     }
 }
-
-
