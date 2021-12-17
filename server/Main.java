0e7931c34eaf6e02b9037a97aed646a579d427b3
vyesman <86164269+doomsayer14@users.noreply.github.com>
2021-12-17 15:32:58 +0200

    2/6 done ✓

--- server/Main.java
+++ server/Main.java
@@ -9,19 +9,17 @@ class Main {
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(PORT)) {
             System.out.println("Server started!");
-            while (true) {
-                try (
-                        Socket socket = server.accept(); // accepting a new client
-                        DataInputStream input = new DataInputStream(socket.getInputStream());
-                        DataOutputStream output = new DataOutputStream(socket.getOutputStream())
-                ) {
-                    String msg = input.readUTF(); // reading a message
-                    System.out.println("Received: " + msg);
-                    String arr[] = msg.split(" ");
-                    msg = "A record # " + arr[arr.length - 1] + " was sent";
-                    output.writeUTF(msg); // resend it to the client
-                    System.out.println("Sent: " + msg);
-                }
+            try (
+                    Socket socket = server.accept(); // accepting a new client
+                    DataInputStream input = new DataInputStream(socket.getInputStream());
+                    DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+            ) {
+                String msg = input.readUTF(); // reading a message
+                System.out.println("Received: " + msg);
+                String[] arr = msg.split(" ");
+                msg = "A record # " + arr[arr.length - 1] + " was sent!";
+                output.writeUTF(msg); // resend it to the client
+                System.out.println("Sent: " + msg);
             }
         } catch (IOException e) {
             e.printStackTrace();
