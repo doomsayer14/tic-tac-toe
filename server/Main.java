1df5a26e970b9b3be047cb5a7491d58db106d2f0
vyesman <86164269+doomsayer14@users.noreply.github.com>
2021-12-17 13:54:25 +0200

    2/6 not done
    Wrong answer in test #1
    
    After the client connects to the server, the server output should contain 3 lines!

--- server/Main.java
+++ server/Main.java
@@ -1,11 +1,30 @@
 package server;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.*;
 
-public class Main {
+class Main {
+    private static final int PORT = 34522;
 
-    public static void main(String[] args) throws IOException {
-        //start server
-        new client.Main().start();
+    public static void main(String[] args) {
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            System.out.println("Server started!");
+            while (true) {
+                try (
+                        Socket socket = server.accept(); // accepting a new client
+                        DataInputStream input = new DataInputStream(socket.getInputStream());
+                        DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+                ) {
+                    String msg = input.readUTF(); // reading a message
+                    System.out.println("Received: " + msg);
+                    String arr[] = msg.split(" ");
+                    msg = "A record # " + arr[arr.length - 1] + " was sent";
+                    output.writeUTF(msg); // resend it to the client
+                    System.out.println("Sent: " + msg);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
